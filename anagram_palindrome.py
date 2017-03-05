7bbe02ab0301680911357ac51243ac1204c572d3
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-03-04 23:53:36 -0800

    🌈 anagram of palindrome ⭐️

--- anagram_palindrome.py
+++ anagram_palindrome.py
@@ -6,6 +6,7 @@ def anagram_of_palindrome(exp):
             anagram_set.remove(char)
         else:
             anagram_set.add(char)
-    return len(exp) < 2
+    return len(anagram_set) < 2
 
 print anagram_of_palindrome('red')
+print anagram_of_palindrome('tacocat')
