f23f9ef689633c26b553904cb870050160de4434
Patricia Arbona <arbonap@gmail.com>
2017-07-18 21:38:53 -0500

    Detect if a LL has duplicates or has a cycle

---
+++ detect_ll_cycle.rb
@@ -0,0 +1,42 @@
+require 'set'
+
+class Node
+  attr_accessor :value, :next
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
+class LinkedList
+  attr_accessor :head
+  def initialize(value)
+    @head = Node.new(value)
+  end
+
+  def has_duplicate?(head)
+    seen = Set.new
+    current = @head
+    while current.next != nil
+      if !seen.include? current.value
+        seen.add(current.value)
+      else
+        return true
+      end
+    end
+    false
+  end
+
+  def has_cycle?(first_node)
+    fast_runner = first_node
+    slow_runner = first_node
+
+    while fast_runner != nil && fast_runner.next != nil
+      slow_runner = slow_runner.next
+      fast_runner = fast_runner.next.next
+
+      return true if slow_runner == fast_runner
+    end
+    false
+  end
+end
